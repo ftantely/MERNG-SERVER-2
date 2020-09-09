1ad635d2c656164f255ca2ee8fcbc2d69cf34873
Tantely Rambeloson <ftantely@gmail.com>
2020-09-09 15:26:00 -0600

    change db

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB:'mongodb+srv://ftantely2:test123@cluster0.alj47.mongodb.net/MERNG-SERVER-2?retryWrites=true&w=majority',
+    MONGODB:'mongodb+srv://ftantely2:test123@cluster0.alj47.mongodb.net/merngserver2?retryWrites=true&w=majority',
     SECRET_KEY:'My secret key'
 }
